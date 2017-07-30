fdf2ed800a9c7569a68cb4ec2ba26eab8795b734
unknown <nkitanand@gmail.com>
2017-07-30 22:23:55 +0530

    Add winning and losing condition.

--- tictactoe.js
+++ tictactoe.js
@@ -9,6 +9,12 @@ var moveCount = 0;
 var validLoc = [1,2,3,4,5,6,7,8,9];
 var humanPos = [];
 var cpuPos   = [];
+var boardState = [-1,
+				  0,0,0,
+				  0,0,0,
+				  0,0,0];	// -1 to invalidate zero index
+var humanFlag = 1;
+var cpuFlag   = 2;
 
 $(document).ready(function(){
 	addEvent();
@@ -39,12 +45,13 @@ var play = function(player, id) {
 			var index = indexOf(validLoc, id);
 			validLoc.splice(index, 1);
 			humanPos.push(parseInt(id));
+			boardState[id] = humanFlag;
 
 			console.log(index);
 			console.log(validLoc);
 			console.log(humanPos);
 
-			if (!isGameOver()) {
+			if (!isGameOver(humanFlag)) {
 				// Give the turn to CPU bot
 				play(cpu, 0);
 			}
@@ -65,12 +72,13 @@ var play = function(player, id) {
 		var index = indexOf(validLoc, id);
 		validLoc.splice(index, 1);
 		cpuPos.push(parseInt(id));
+		boardState[id] = cpuFlag;
 
 		console.log(index);
 		console.log(validLoc);
 		console.log(cpuPos);
 
-		isGameOver();
+		isGameOver(cpuFlag);
 	}
 }
 
@@ -85,13 +93,45 @@ var indexOf = function(arr, ele) {
 	return index;
 }
 
-var isGameOver = function() {
+var isGameOver = function(playerFlag) {
 	
 	if (parseInt(moveCount) == 9) {
 		window.setTimeout(function() {
-			alert("TIE");
-			//window.location.reload(false);
+			document.getElementById("result").innerHTML = "It's a TIE";
+			alert("It's a TIE!");
+			window.location.reload(false);
 		}, 200);
+		return true;
+	}
+
+	if (captured(playerFlag)) {
+		window.setTimeout(function() {
+			if (playerFlag == humanFlag) { 
+				document.getElementById("result").innerHTML = "You WON!";
+				alert("You WON! But Smart Bot is coming soon to kick your S");
+			}
+			else if (playerFlag == cpuFlag) {
+				document.getElementById("result").innerHTML = "You LOSE!";
+				alert("You LOSE!");
+			}
+			window.location.reload(false);
+		}, 200);
+		return true;
+	}
+
+	return false;
+}
+
+var captured = function(playerFlag) {
+
+	if ( (boardState[1] == playerFlag && boardState[2] == playerFlag && boardState[3] == playerFlag)
+		 || (boardState[4] == playerFlag && boardState[5] == playerFlag && boardState[6] == playerFlag)
+		 || (boardState[7] == playerFlag && boardState[8] == playerFlag && boardState[9] == playerFlag)
+		 || (boardState[1] == playerFlag && boardState[4] == playerFlag && boardState[7] == playerFlag)
+		 || (boardState[2] == playerFlag && boardState[5] == playerFlag && boardState[8] == playerFlag)
+		 || (boardState[3] == playerFlag && boardState[6] == playerFlag && boardState[9] == playerFlag)
+		 || (boardState[1] == playerFlag && boardState[5] == playerFlag && boardState[9] == playerFlag)
+		 || (boardState[3] == playerFlag && boardState[5] == playerFlag && boardState[7] == playerFlag) ) {
 		return true;
 	}
 	return false;
